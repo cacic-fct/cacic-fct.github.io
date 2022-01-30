35c0cc04531f0a3f6fc9210b8be4ae8cd5fc9363
Yudi <renanyaguinuma@hotmail.com>
2022-01-30 14:50:32 -0300

    Adicionar glossário

---
+++ src/app/page-manual-calouro/components/manual-glossario/manual-glossario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-glossario',
+  templateUrl: './manual-glossario.component.html',
+  styleUrls: [
+    './manual-glossario.component.scss',
+    '../../page-manual-calouro.page.scss',
+  ],
+})
+export class ManualGlossarioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
