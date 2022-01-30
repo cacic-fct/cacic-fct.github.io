0276c8cf3e1d0ebc6ffff3c87f86560c2669d80b
Yudi <renanyaguinuma@hotmail.com>
2022-01-29 23:11:14 -0300

    Adicionar css da página

--- src/app/page-manual-calouro/components/manual-operadoras/manual-operadoras.component.ts
+++ src/app/page-manual-calouro/components/manual-operadoras/manual-operadoras.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manual-operadoras',
   templateUrl: './manual-operadoras.component.html',
-  styleUrls: ['./manual-operadoras.component.scss'],
+  styleUrls: [
+    './manual-operadoras.component.scss',
+    '../../page-manual-calouro.page.scss',
+  ],
 })
 export class ManualOperadorasComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
