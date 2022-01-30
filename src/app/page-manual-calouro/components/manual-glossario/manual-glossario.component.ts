d549880e3abcbfe1540bcc902fc8ce29cd6554f7
Yudi <renanyaguinuma@hotmail.com>
2022-01-30 17:47:41 -0300

    Glossário Softcoded

--- src/app/page-manual-calouro/components/manual-glossario/manual-glossario.component.ts
+++ src/app/page-manual-calouro/components/manual-glossario/manual-glossario.component.ts
@@ -9,7 +9,63 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class ManualGlossarioComponent implements OnInit {
+  dictionary = [
+    {
+      term: 'ACC',
+      definition: 'Atividades complementares ao curso.',
+    },
+    {
+      term: 'TG',
+      definition: 'Trabalho de gradução. Também conhecido como TCC.',
+    },
+    {
+      term: 'TG',
+      definition:
+        'Tiro de guerra. Uma forma de prestar o serviço militar obrigatório.',
+    },
+    {
+      term: 'TCC',
+      definition: 'Trabalho de conclusão de curso.',
+    },
+    {
+      term: 'CR',
+      definition: 'Coeficiente de rendimento.',
+    },
+    {
+      term: 'IC',
+      definition: 'Iniciação científica.',
+    },
+    {
+      term: 'PIBIC',
+      definition: 'Programa institucional de bolsas de iniciação científica.',
+    },
+    {
+      term: 'FAPESP',
+      definition: 'Fundação de amparo à pesquisa do estado de São Paulo.',
+    },
+    {
+      term: 'CAPES',
+      definition:
+        'Coordenação de Aperfeiçoamento de Pessoal de Nível Superior.',
+    },
+    {
+      term: 'PIBITI',
+      definition:
+        'Programa institucional de bolsas de iniciação em desenvolvimento tecnológico e inovação.',
+    },
+    {
+      term: 'DP',
+      definition: 'Dependência. Quando se fica retido em uma disciplina.',
+    },
+    {
+      term: 'RER',
+      definition: 'Regime de recuperação especial.',
+    },
+  ];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dictionary.sort((a, b) => (a.term > b.term ? 1 : -1));
+  }
 }
