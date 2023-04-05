da2837bbb4f36f1486510304cc3e2c31f854250b
Yudi <renanyaguinuma@hotmail.com>
2023-04-04 22:30:18 -0300

    Update version

--- src/app/shared/services/global-constants.service.ts
+++ src/app/shared/services/global-constants.service.ts
@@ -2,5 +2,5 @@ export class GlobalConstants {
   public static appName: string = 'Homepage do CACiC';
 
   /* YYYY.MM.DD.HH.MM */
-  public static appVersion: string = '2023.04.04.21.14';
+  public static appVersion: string = '2023.04.04.22.30';
 }
