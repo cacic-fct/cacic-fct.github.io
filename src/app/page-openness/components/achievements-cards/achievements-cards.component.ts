d0b982ea8c811c9c0a8c554d270796b8c749543f
Yudi <renanyaguinuma@hotmail.com>
2023-05-01 18:06:46 -0300

    Archive Margaret Hamilton Slate

--- src/app/page-openness/components/achievements-cards/achievements-cards.component.ts
+++ src/app/page-openness/components/achievements-cards/achievements-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-achievements-cards',
@@ -6,83 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievements-cards.component.scss'],
 })
 export class AchievementsCardsComponent implements OnInit {
-  public items = [
-    {
-      image: '2021/code_talk.webp',
-      title: 'Code Talk',
-      content:
-        'Circuito de bate-papos, palestras e minicursos com convidados especiais.',
-      url: 'https://instagram.com/p/Cau2R-quc-k',
-    },
-    {
-      image: '2021/fct_app.webp',
-      title: 'FCT App',
-      content:
-        'Aplicativo para facilitar a vida dos alunos da FCT, com o acesso aos eventos do câmpus. Tem alto potencial de expansão.',
-      url: 'https://fct-pp.web.app',
-    },
-    {
-      image: '2021/manual_calouro.webp',
-      title: 'Manual do calouro',
-      content:
-        'Fala sobre a FCT-Unesp, o movimento estudantil, a cidade de Presidente Prudente e muito mais! Está aberto a contribuições.',
-      url: 'https://fct-pp.web.app/manual-do-calouro',
-    },
-    {
-      image: '2021/recepcao_calouros.webp',
-      title: 'Recepção dos calouros',
-      content:
-        'Agradeçemos a Filipe Nava, Manoela Frattini, Raphael Esposti e a todos os outros que nos auxiliaram.',
-    },
-    {
-      image: '',
-      title: 'Homepage do CACiC',
-      content: 'Sim, este site.',
-      url: '',
-    },
-    {
-      image: '2021/vacinacao_sisgrad.webp',
-      title: 'Envio do comprovante de vacinação',
-      content:
-        'Em primeira mão, orientamos os alunos a realizarem o cadastro do comprovante.',
-      url: 'https://instagram.com/p/CYufQC0vwJL',
-    },
-    {
-      image: 'unesp.png',
-      title: 'Regularização no conselho de curso',
-      content: 'Regularização da situação discente no conselho.',
-    },
-    {
-      image: '2021/dezembro_vermelho.webp',
-      title: 'Dezembro vermelho',
-      content: 'Série de posts conscientizando sobre HIV/AIDS e outras ISTs.',
-      url: 'https://instagram.com/p/CX3xXzXLATD',
-    },
-    {
-      image: '2021/rematricula_calouros.webp',
-      title: 'Campanha de rematrícula dos calouros',
-      content: 'Post e lembretes nos grupos de WhatsApp.',
-      url: 'https://instagram.com/p/CVdSykWLIbD',
-    },
-    {
-      image: '',
-      title: 'Manutenção das mídias sociais',
-      content:
-        'Manutenção realizada nas páginas do Facebook e Instagram, além do perfil no Facebook.',
-    },
-    {
-      image: '2021/hacktoberfest_2021.webp',
-      title: 'Hacktoberfest 2021',
-      content:
-        'Palestra e minicurso com carga horária de 10 horas. Confecção e envio dos certificados.',
-    },
-    {
-      image: '2021/eleicao.webp',
-      title: 'Processo eleitoral de 2021',
-      content: 'Realizado junto à sua respectiva comissão.',
-      url: 'https://instagram.com/p/CUxgWC9Lixk',
-    },
-  ];
+  @Input() achievements: {
+    image?: string;
+    title: string;
+    content: string;
+    url?: string;
+  }[];
 
   constructor() {}
 
--- src/app/page-openness/components/news-accordion/news-accordion.component.ts
+++ src/app/page-openness/components/news-accordion/news-accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news-accordion',
@@ -6,33 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-accordion.component.scss'],
 })
 export class NewsAccordionComponent implements OnInit {
-  meetings = [
-    {
-      date: '2021-12-05',
-      title: 'Reunião de dezembro',
-      id: '1dZd-FwjXMTGbbs1yu3JNDB6lXjbItYKKMRvrefRXnzU',
-    },
-    {
-      date: '2021-10-27',
-      title: 'Cerimônia de posse da chapa',
-      id: '1m-By1TGhyySP0f52Fm5Z1dqB2XTVMZ9gQpuLIwHoUBw',
-    },
-    {
-      date: '2021-10-25',
-      title: 'Conversa entre as chapas',
-      id: '1Nl2TfOckNuOX5CJcUQUQHgF7eJx5oolMroheElKyAqQ',
-    },
-    {
-      date: '2021-10-08',
-      title: 'Apresentação da chapa Margaret Hamilton',
-      id: '1cknkNev-keAnTDVbz41ZRIj9C19jcVuaWMzSa67Ip6g',
-    },
-    {
-      date: '2021-10-06',
-      title: 'Discussão inicial com os membros da chapa',
-      id: '118Dkj4GWq2E_btrA6q8FX7UWlOrMY2LXpN1wtX5tXxE',
-    },
-  ];
+  @Input() meetings: { date: string; title: string; id: string }[];
+
   constructor() {}
 
   ngOnInit() {
