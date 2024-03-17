fb8bd4bef0c3117ce67a3f0fe97674f14b90da4d
Yudi <12040397+Yudi@users.noreply.github.com>
2024-03-17 01:53:56 -0300

    Move QuickAccess card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 import styles from './index.module.css';
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import AppsIcon from '@mui/icons-material/Apps';
-import GroupIcon from '@mui/icons-material/Group';
-import ArticleIcon from '@mui/icons-material/Article';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/material';
 
 import { members as katieMembers } from '@site/src/shared/components/slates/katie-bouman';
 import SlateGridGenerator from '@site/src/shared/components/slates/slate-grid-generator';
+import QuickAccess from '@site/src/pages/index-components/quick-access';
 
 export default function Home(): JSX.Element {
   return (
@@ -31,51 +17,7 @@ export default function Home(): JSX.Element {
         description="Description will go into a meta tag in <head />">
         <main>
           <Container sx={{ p: 4 }} maxWidth="sm">
-            <Card>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Acesso rápido
-                </Typography>
-
-                <List>
-                  <ListItem disablePadding>
-                    <ListItemButton href="https://www.fct.unesp.br/#!/administracao/graduacao/espaco-do-aluno/">
-                      <ListItemIcon>
-                        <CalendarMonthIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Calendário do câmpus" />
-                    </ListItemButton>
-                  </ListItem>
-
-                  <ListItem disablePadding>
-                    <ListItemButton href="https://fct-pp.web.app/">
-                      <ListItemIcon>
-                        <AppsIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="FCT App" />
-                    </ListItemButton>
-                  </ListItem>
-
-                  <ListItem disablePadding>
-                    <ListItemButton href="https://fct-pp.web.app/calouros">
-                      <ListItemIcon>
-                        <GroupIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Página do calouro" />
-                    </ListItemButton>
-                  </ListItem>
-
-                  <ListItem disablePadding>
-                    <ListItemButton href="https://drive.google.com/file/d/1IkP7qYwvtdDc9CRiKueyjZ8bESRsDUS5/view">
-                      <ListItemIcon>
-                        <ArticleIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Estatuto do CACiC" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </CardContent>
-            </Card>
+            <QuickAccess />
           </Container>
 
           <Container sx={{ p: 2 }} maxWidth="sm">
