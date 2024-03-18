6813163b4c544e5267b2ad87181f3513a74f4769
Yudi <12040397+Yudi@users.noreply.github.com>
2024-03-18 19:47:00 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,35 +9,33 @@ import QuickAccess from '@site/src/pages/index-components/quick-access';
 
 export default function Home(): JSX.Element {
   return (
-    <>
-      <Layout description="Homepage do CACiC">
-        <main>
-          <Container sx={{ p: 4 }} maxWidth="sm">
-            <QuickAccess />
-          </Container>
+    <Layout description="Homepage do CACiC">
+      <main>
+        <Container sx={{ p: 4 }} maxWidth="sm">
+          <QuickAccess />
+        </Container>
 
-          <Container sx={{ p: 2 }} maxWidth="sm">
-            <Typography variant="h5" sx={{ py: 1 }}>
-              O CACiC
-            </Typography>
-            <p>
-              O Centro Acadêmico de Ciência da Computação é o órgão de
-              representação estudantil do curso de Bacharelado em Ciência da
-              Computação da FCT-Unesp. A organização defende os interesses e
-              direitos dos alunos. A direção do CA é escolhida por meio de
-              eleições periódicas, entre chapas de alunos do curso.
-            </p>
-          </Container>
+        <Container sx={{ p: 2 }} maxWidth="sm">
+          <Typography variant="h5" sx={{ py: 1 }}>
+            O CACiC
+          </Typography>
+          <p>
+            O Centro Acadêmico de Ciência da Computação é o órgão de
+            representação estudantil do curso de Bacharelado em Ciência da
+            Computação da FCT-Unesp. A organização defende os interesses e
+            direitos dos alunos. A direção do CA é escolhida por meio de
+            eleições periódicas, entre chapas de alunos do curso.
+          </p>
+        </Container>
 
-          <Container sx={{ p: 2 }} maxWidth="sm">
-            <Typography variant="h5" sx={{ py: 1 }}>
-              Chapa Katie Bouman
-            </Typography>
-            <SlateGridGenerator slateMembers={katieMembers} />
-          </Container>
-        </main>
-        <Box sx={{ py: 4 }} />
-      </Layout>
-    </>
+        <Container sx={{ p: 2 }} maxWidth="sm">
+          <Typography variant="h5" sx={{ py: 1 }}>
+            Chapa Katie Bouman
+          </Typography>
+          <SlateGridGenerator slateMembers={katieMembers} />
+        </Container>
+      </main>
+      <Box sx={{ py: 4 }} />
+    </Layout>
   );
 }
