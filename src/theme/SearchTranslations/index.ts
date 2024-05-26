f3440300c3eaaff4d4b81abaf6edeb67477bc74c
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-26 13:56:38 -0300

    Add more translations

--- src/theme/SearchTranslations/index.ts
+++ src/theme/SearchTranslations/index.ts
@@ -19,138 +19,138 @@ const translations: DocSearchTranslations & { placeholder: string } = {
     searchBox: {
       resetButtonTitle: translate({
         id: 'theme.SearchModal.searchBox.resetButtonTitle',
-        message: 'Clear the query',
+        message: 'Limpar a busca',
         description: 'The label and ARIA label for search box reset button',
       }),
       resetButtonAriaLabel: translate({
         id: 'theme.SearchModal.searchBox.resetButtonTitle',
-        message: 'Clear the query',
+        message: 'Limpar a busca',
         description: 'The label and ARIA label for search box reset button',
       }),
       cancelButtonText: translate({
         id: 'theme.SearchModal.searchBox.cancelButtonText',
-        message: 'Cancel',
+        message: 'Cancelar',
         description: 'The label and ARIA label for search box cancel button',
       }),
       cancelButtonAriaLabel: translate({
         id: 'theme.SearchModal.searchBox.cancelButtonText',
-        message: 'Cancel',
+        message: 'Cancelar',
         description: 'The label and ARIA label for search box cancel button',
       }),
     },
     startScreen: {
       recentSearchesTitle: translate({
         id: 'theme.SearchModal.startScreen.recentSearchesTitle',
-        message: 'Recent',
+        message: 'Recente',
         description: 'The title for recent searches',
       }),
       noRecentSearchesText: translate({
         id: 'theme.SearchModal.startScreen.noRecentSearchesText',
-        message: 'No recent searches',
+        message: 'Sem buscas recentes',
         description: 'The text when no recent searches',
       }),
       saveRecentSearchButtonTitle: translate({
         id: 'theme.SearchModal.startScreen.saveRecentSearchButtonTitle',
-        message: 'Save this search',
+        message: 'Salvar esta busca recente',
         description: 'The label for save recent search button',
       }),
       removeRecentSearchButtonTitle: translate({
         id: 'theme.SearchModal.startScreen.removeRecentSearchButtonTitle',
-        message: 'Remove this search from history',
+        message: 'Remover esta busca do histórico',
         description: 'The label for remove recent search button',
       }),
       favoriteSearchesTitle: translate({
         id: 'theme.SearchModal.startScreen.favoriteSearchesTitle',
-        message: 'Favorite',
+        message: 'Favorito',
         description: 'The title for favorite searches',
       }),
       removeFavoriteSearchButtonTitle: translate({
         id: 'theme.SearchModal.startScreen.removeFavoriteSearchButtonTitle',
-        message: 'Remove this search from favorites',
+        message: 'remover esta busca dos favoritos',
         description: 'The label for remove favorite search button',
       }),
     },
     errorScreen: {
       titleText: translate({
         id: 'theme.SearchModal.errorScreen.titleText',
-        message: 'Unable to fetch results',
+        message: 'Não foi possível carregar os resultados.',
         description: 'The title for error screen of search modal',
       }),
       helpText: translate({
         id: 'theme.SearchModal.errorScreen.helpText',
-        message: 'You might want to check your network connection.',
+        message: 'Verifique sua conexão com a internet.',
         description: 'The help text for error screen of search modal',
       }),
     },
     footer: {
       selectText: translate({
         id: 'theme.SearchModal.footer.selectText',
-        message: 'to select',
+        message: 'para selecionar',
         description: 'The explanatory text of the action for the enter key',
       }),
       selectKeyAriaLabel: translate({
         id: 'theme.SearchModal.footer.selectKeyAriaLabel',
-        message: 'Enter key',
+        message: 'Tecla Enter',
         description:
           'The ARIA label for the Enter key button that makes the selection',
       }),
       navigateText: translate({
         id: 'theme.SearchModal.footer.navigateText',
-        message: 'to navigate',
+        message: 'para navegar',
         description:
           'The explanatory text of the action for the Arrow up and Arrow down key',
       }),
       navigateUpKeyAriaLabel: translate({
         id: 'theme.SearchModal.footer.navigateUpKeyAriaLabel',
-        message: 'Arrow up',
+        message: 'Seta para cima',
         description:
           'The ARIA label for the Arrow up key button that makes the navigation',
       }),
       navigateDownKeyAriaLabel: translate({
         id: 'theme.SearchModal.footer.navigateDownKeyAriaLabel',
-        message: 'Arrow down',
+        message: 'Seta para baixo',
         description:
           'The ARIA label for the Arrow down key button that makes the navigation',
       }),
       closeText: translate({
         id: 'theme.SearchModal.footer.closeText',
-        message: 'to close',
+        message: 'para fechar',
         description: 'The explanatory text of the action for Escape key',
       }),
       closeKeyAriaLabel: translate({
         id: 'theme.SearchModal.footer.closeKeyAriaLabel',
-        message: 'Escape key',
+        message: 'Tecla ESC',
         description:
           'The ARIA label for the Escape key button that close the modal',
       }),
       searchByText: translate({
         id: 'theme.SearchModal.footer.searchByText',
-        message: 'Search by',
+        message: 'Buscar por',
         description: 'The text explain that the search is making by Algolia',
       }),
     },
     noResultsScreen: {
       noResultsText: translate({
         id: 'theme.SearchModal.noResultsScreen.noResultsText',
-        message: 'No results for',
+        message: 'Sem resultados para',
         description:
           'The text explains that there are no results for the following search',
       }),
       suggestedQueryText: translate({
         id: 'theme.SearchModal.noResultsScreen.suggestedQueryText',
-        message: 'Try searching for',
+        message: 'Tente buscar por',
         description:
           'The text for the suggested query when no results are found for the following search',
       }),
       reportMissingResultsText: translate({
         id: 'theme.SearchModal.noResultsScreen.reportMissingResultsText',
-        message: 'Believe this query should return results?',
+        message: 'Achou que faltam resultados?',
         description:
           'The text for the question where the user thinks there are missing results',
       }),
       reportMissingResultsLinkText: translate({
         id: 'theme.SearchModal.noResultsScreen.reportMissingResultsLinkText',
-        message: 'Let us know.',
+        message: 'Informe-nos.',
         description: 'The text for the link to report missing results',
       }),
     },
