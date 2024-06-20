779d5e7ecf93198435ff0a0e7873b6423441ae9b
Yudi <12040397+Yudi@users.noreply.github.com>
2024-06-20 16:36:07 -0300

    Use BrowserOnly

--- src/components/live/youtubeBroadcast.tsx
+++ src/components/live/youtubeBroadcast.tsx
@@ -1,4 +1,4 @@
-const hostname = window.location.hostname;
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import styles from './youtubeBroadcast.module.css';
 
@@ -14,19 +14,28 @@ export default function YouTubeBroadcast({
           return (
             <>
               <div className={styles.container}>
-                <div className={styles.video_wrapper}>
-                  <iframe
-                    className={styles.video}
-                    src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
-                    title="Transmissão do YouTube"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                </div>
-
-                <div className={styles.chat_wrapper}>
-                  <iframe
-                    className={styles.chat}
-                    src={`https://www.youtube.com/live_chat?v=${videoId}&amp;embed_domain=${hostname}`}></iframe>
-                </div>
+                <BrowserOnly>
+                  {() => {
+                    const hostname = window.location.hostname;
+                    return (
+                      <>
+                        {' '}
+                        <div className={styles.video_wrapper}>
+                          <iframe
+                            className={styles.video}
+                            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
+                            title="Transmissão do YouTube"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                        </div>
+                        <div className={styles.chat_wrapper}>
+                          <iframe
+                            className={styles.chat}
+                            src={`https://www.youtube.com/live_chat?v=${videoId}&amp;embed_domain=${hostname}`}></iframe>
+                        </div>
+                      </>
+                    );
+                  }}
+                </BrowserOnly>
               </div>
             </>
           );
