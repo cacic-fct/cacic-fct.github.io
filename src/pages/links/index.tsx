806213437afd794c1b5aff41d5c87450acf90d63
Yudi <12040397+Yudi@users.noreply.github.com>
2025-02-01 14:23:32 -0300

    Formatting

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -6,59 +6,64 @@ import LinkList from '@site/src/components/links/linkList';
 import { CardList, ListType } from '@site/src/components/links/linkList';
 import { Container } from '@mui/material';
 
-/* Example
- * Para adicionar um item à árvore de links, edite as variáveis linkList e cardList que estão fora do comentário.
+/* Para adicionar um item à árvore de links, edite as variáveis linkList e cardList que estão fora do comentário.
+ * Se você não possui permissão no repositório, abra um pull request com as alterações.
  *
- * linkList: Lista de links
- ** title: Título do item
- ** icon: Ícone MUI
- ** url: URL do item
- ** content: Descrição do item ou null
+ * Descrição dos campos:
+ ** linkList: Lista de links {
+ **   title: Título do item,
+ **   icon: Ícone MUI,
+ **   url: URL do item,
+ **   content: Descrição do item ou null
+ ** }
+ **
+ ** cardList: Lista de cards {
+ **   title: Título do card ou null
+ **   content: Lista de links
+ **   img: Imagem do card (opcional)
+ **   src: URL da imagem
+ **   alt: Texto alternativo da imagem
+ ** }
  *
- * cardList: Lista de cards
- ** title: Título do card
- ** content: Lista de links
- ** img: Imagem do card (opcional)
- *** src: URL da imagem
- *** alt: Texto alternativo da imagem
+ * Exemplo:
 
-      const linkList1 = [
-        {
-          title: 'Plenária do MUP – Inscrições',
-          icon: 'document-text',
-          url: 'https://forms.gle/kd6AKGDaKRnphDYW8',
-          content:
-            '22/01 e 23/01 – Ciência e Tecnologia no Brasil: Para onde vai o que produzimos? E para onde deve ir?',
-        },
-      ];
+ const linkList1 = [
+   {
+     title: 'Plenária do MUP – Inscrições',
+     icon: 'document-text',
+     url: 'https://forms.gle/kd6AKGDaKRnphDYW8',
+     content:
+       '22/01 e 23/01 – Ciência e Tecnologia no Brasil: Para onde vai o que produzimos? E para onde deve ir?',
+  },
+ ];
   
-      const linkList2 = [
-        {
-          title: 'Site do CACiC',
-          icon: 'school',
-          url: 'https://cacic.dev.br',
-          content: null,
-        },
-      ];
+ const linkList2 = [
+   {
+     title: 'Site do CACiC',
+     icon: 'school',
+     url: 'https://cacic.dev.br',
+     content: null,
+   },
+ ];
 
-      const cardList = [
-        {
-          title: null,
-          content: null,
-          img: {
-            src: 'assets/linktree/banner.webp',
-            alt: 'Banner da plenária',
-          },
-        },
-        {
-          title: 'Movimento estudantil',
-          content: linkList1,
-        },
-        {
-          title: 'CACiC',
-          content: linkList2,
-        },
-      ];
+ const cardList = [
+   {
+     title: null,
+     content: null,
+     img: {
+       src: 'assets/linktree/banner.webp',
+       alt: 'Banner da plenária',
+     },
+   },
+   {
+     title: 'Movimento estudantil',
+     content: linkList1,
+   },
+   {
+     title: 'CACiC',
+     content: linkList2,
+   },
+ ];
 */
 
 const linkList: ListType[] = [];
