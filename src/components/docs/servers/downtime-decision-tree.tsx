956db24e0fe98b9fe4abed3e5b63fc54b16a35ed
Yudi <12040397+Yudi@users.noreply.github.com>
2025-04-21 02:58:11 -0300

    Cleanup

--- src/components/docs/servers/downtime-decision-tree.tsx
+++ src/components/docs/servers/downtime-decision-tree.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Card, CardContent, Typography, Link } from '@mui/material';
 
 type StepKeys =
