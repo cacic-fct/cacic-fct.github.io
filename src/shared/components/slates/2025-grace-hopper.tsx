479d9785b18c87c5aad650649bc09c8548eac65e
Abigail Sayury <abigail.s.nakashima@unesp.br>
2025-11-05 17:38:49 -0300

    Add Grace Hopper slate and grid

---
+++ src/shared/components/slates/2025-grace-hopper.tsx
@@ -0,0 +1,34 @@
+import { SlateMember } from '@site/src/shared/components/slates/slate-grid-generator';
+
+export const members: SlateMember[] = [
+    {
+        name: 'Abigail Nakashima',
+        role: 'Presidente',
+        avatar: require('@site/static/people/abigail-nakashima.avif').default,
+    },
+    {
+        name: 'Miguel Moret',
+        role: 'Vice-presidente',
+        avatar: require('@site/static/people/avatar-placeholder.webp').default,
+    },
+    {
+        name: 'Cristian Eidi',
+        role: 'Comunicação',
+        avatar:require('@site/static/people/cristian-eidi.avif').default,
+    },
+    {
+        name: 'Daniel Pádua',
+        role: 'Eventos',
+        avatar:require('@site/static/people/avatar-placeholder.webp').default,
+    },
+    {
+        name: 'Felipe Nava',
+        role: 'Relações públicas',
+        avatar:require('@site/static/people/avatar-placeholder.webp').default,
+    },
+    {
+        name: 'Paulo Lima',
+        role: 'Finanças',
+        avatar:require('@site/static/people/avatar-placeholder.webp').default,
+    },
+]
