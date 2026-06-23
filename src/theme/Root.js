222a9b847916200a312cbb462335e04350046ff9
Yudi <12040397+Yudi@users.noreply.github.com>
2026-06-23 17:35:16 -0300

    Cookie banner

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,7 +1,24 @@
 // Attribution:
 // https://webreaper.dev/posts/material-ui-theme-with-docusaurus/
 
+import { useEffect } from 'react';
+import { createCookieBanner } from '@cacic-fct/account-manager-cookie-banner';
+import '@cacic-fct/account-manager-cookie-banner/styles.css';
+
 export default function Root({ children }) {
+  useEffect(() => {
+    const banner = createCookieBanner({
+      privacyPolicyUrl: 'https://cacic.dev.br/legal/privacy-policy',
+      onAccept: () => {
+        window.dispatchEvent(new Event('cookieBannerAccepted'));
+      },
+    });
+
+    return () => {
+      banner.destroy();
+    };
+  }, []);
+
   return (
     <>
       {/* <InitColorSchemeScript /> */}
